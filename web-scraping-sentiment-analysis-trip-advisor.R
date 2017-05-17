5f8df19997fe2914283390484a08094db7e8db26
Priyank Agrawal <agrawal.pr@husky.neu.edu>
2017-05-17 19:08:14 -0400

    new csv and changes in r file

--- web-scraping-sentiment-analysis-trip-advisor.R
+++ web-scraping-sentiment-analysis-trip-advisor.R
@@ -3,7 +3,7 @@ library(tm)
 library(stringr)
 library(rvest)
 
-# Web scraping live reviews about JW Marriott hotel from Trip-Advisor
+# Web scraping live reviews of JW Marriott hotel from Trip-Advisor
 df <- data.frame(Date=as.Date(character()), File=character(), User=character(), stringsAsFactors=FALSE)
 x <- 0
 for(i in c(1:100)){
@@ -33,16 +33,18 @@ for(i in c(1:100)){
   }
 }
 
-write.csv(df, "C:/Users/agraw/Desktop/ADS/Final Project/tripadvisor_reviews.csv")
+# Write the dataframe to a csv
+write.csv(df, "tripadvisor_reviews.csv")
 
 # Sentiment Analysis of Hotel Reviews on Trip Advisor
-# Load the amazon review data and explore
-trip <- read.csv("C:/Users/agraw/Desktop/ADS/Final Project/tripadvisor_reviews.csv")
+
+# Load the same csv
+trip <- read.csv("tripadvisor_reviews.csv")
 hotel_reviews <- as.character(trip$review)
 
 # Load the positive and negative lexicon data and explore
-positive_lexicon <- read.csv("C:/Users/agraw/Desktop/ADS/Final Project/positive-lexicon.txt")
-negative_lexicon <- read.csv("C:/Users/agraw/Desktop/ADS/Final Project/negative-lexicon.txt")
+positive_lexicon <- read.csv("positive-lexicon.txt")
+negative_lexicon <- read.csv("negative-lexicon.txt")
 
 # Making a corpus out of the hotel reviews
 reviews_corpus <- Corpus(VectorSource(hotel_reviews))
@@ -61,7 +63,7 @@ filtered_corpus_to_lower <- tm_map(filtered_corpus_no_whitespace, content_transf
 inspect(filtered_corpus_to_lower)
 
 # Load the stop words text file and explore
-stop_words <- read.csv("C:/Users/agraw/Desktop/ADS/Final Project/stopwords_en.txt")
+stop_words <- read.csv("stopwords_en.txt")
 
 # Remove stop words of the external file from the corpus and whitespaces again and inspect
 stopwords_vec <- as.data.frame(stop_words)
@@ -79,7 +81,7 @@ stemmed_corpus <- tm_map(final_corpus, stemDocument)
 
 # Building a term document matrix of the stemmed corpus
 TDM_corpus <- TermDocumentMatrix(stemmed_corpus)
-findFreqTerms(TDM_corpus, 5)                    ##terms occurring with a minimum frequency of 5
+findFreqTerms(TDM_corpus, 5)                    # terms occurring with a minimum frequency of 5
 
 # Calculating the count and percentage of total positive and negative words in each review and
 # Labeling each review as either negative or positive
@@ -88,7 +90,9 @@ total_neg_count <- 0
 pos_count_vector <- c()
 neg_count_vector <- c()
 
-for(i in 1:1000){
+size <- length(stemmed_corpus)
+
+for(i in 1:size){
   corpus_words<- list(strsplit(stemmed_corpus[[i]]$content, split = " "))
   #print(intersect(unlist(corpus_words), unlist(positive_lexicon))) ## positive words in current review
   pos_count <- length(intersect(unlist(corpus_words), unlist(positive_lexicon)))
@@ -112,14 +116,13 @@ for(i in 1:1000){
 
 # Sentiment score of each review and visualizing using boxplot
 counts <- data.frame(pos_count_vector, neg_count_vector)
-sentiment <- data.frame(c(1:1000),(pos_count_vector - neg_count_vector) / (pos_count_vector + neg_count_vector))
-sentiment
-boxplot(sentiment$X.pos_count_vector...neg_count_vector...pos_count_vector...neg_count_vector.[400:500]~sentiment$c.1.1000.[400:500])
+sentiment <- data.frame(c(1:size),(pos_count_vector - neg_count_vector) / (pos_count_vector + neg_count_vector))
+boxplot(sentiment$X.pos_count_vector...neg_count_vector...pos_count_vector...neg_count_vector.[0:100]~sentiment$c.1.size.[0:100])
 
 # Visualiztion of positive and negative count of single review
-singe_review <- c(counts$pos_count_vector[1], counts$neg_count_vector[1])
-barplot(t(as.data.frame(singe_review)), ylab = "Count", xlab = "Positve and Negative",  
-        main = "Positive v/s Negative count of each Review")
+singe_review <- c(counts$pos_count_vector[3], counts$neg_count_vector[3])
+barplot(t(as.data.frame(singe_review)), ylab = "Count", xlab = "Positve v/s Negative",  
+        main = "Positive and Negative words in Review")
 
 # Calculating overall percentage of positive and negative words of all the reviews
 total_pos_count                                  ## overall positive count
@@ -133,10 +136,10 @@ overall_negative_percentage                      ## overall negative percentage
 # Visualization of positive and negative word count for all the reviews
 review_count_frame <- data.frame(matrix(c(pos_count_vector, neg_count_vector), nrow = 100, ncol = 2))
 colnames(review_count_frame) <- c("Positive Word Count", "Negative Word Count")
-barplot(review_count_frame$`Positive Word Count`, ylab = "Count", xlab = "Reviews from 1 to 100",  
-        main = "Positive word count of Reviews", col="lightblue")
-barplot(review_count_frame$`Negative Word Count`, ylab = "Count", xlab = "Reviews from 1 to 100",  
-        main = "Negative word count of Reviews", col="lightblue")
+barplot(review_count_frame$`Positive Word Count`, ylab = "Positive Word Count", xlab = "Reviews from 1 to 100",  
+        main = "Positive words in Reviews", col="lightblue")
+barplot(review_count_frame$`Negative Word Count`, ylab = "Negative Word Count", xlab = "Reviews from 1 to 100",  
+        main = "Negative words in Reviews", col="lightblue")
 
 # Visualization of Overall positive and negative reviews
 percent_vec <- c(overall_positive_percentage, overall_negative_percentage)
